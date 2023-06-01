b07df8725938f850d61ee3ea709e0fdbd3fb95fb
james yates <yates_7@hotmail.com>
2023-06-01 18:15:21 +0100

    refactored  convert method but needs to be separated dependencies

--- lib/Pig_Latin_Converter.rb
+++ lib/Pig_Latin_Converter.rb
@@ -10,25 +10,25 @@ class PigLatinConverter
 
   def self.convert_word(word)
     vowels = ["a", "e", "i", "o", "u"]
-    upper_vowels = ["A", "E", "I", "O", "U"]
     first_letter = word[0]
-    if vowels.include?(first_letter) || upper_vowels.include?(first_letter)
-      word.append("way")
-    else
-      consonants = []
-      word.each_with_index do |char, index|
-        if vowels.include?(char)
-          vowel_index = index
-          break
-        end
-      index = 0
-      while index < vowel_index do
-        consonants.append(word[i])
-        index += 1
-      end
-      "#{word[consonants.length..-1] + consonants.join + "ay"}"
+    return word.append("way") if vowels.include?(first_letter.downcase)
+
+    consonants = []
+    vowel_index = 0
+    word.each_with_index do |char, index|
+      if vowels.include?(char)
+        vowel_index = index
+        break
       end
     end
+      index = 0
+      while index < vowel_index do
+        consonants.append(word[index])
+        index += 1
+      end
+      word[consonants.length..-1] + consonants + ["ay"]
   end
 
 end
+
+puts PigLatinConverter.convert("James")
